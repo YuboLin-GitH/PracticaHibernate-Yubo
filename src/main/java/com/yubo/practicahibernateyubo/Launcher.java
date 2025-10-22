a35755d074f2b1582f5f5119d01ca83b6167fa27
YuboLin-GitH <yubo.lin@educa.jcyl.es>
2025-10-22 08:36:22 +0200

    proyecto creado

---
+++ src/main/java/com/yubo/practicahibernateyubo/Launcher.java
@@ -0,0 +1,9 @@
+package com.yubo.practicahibernateyubo;
+
+import javafx.application.Application;
+
+public class Launcher {
+    public static void main(String[] args) {
+        Application.launch(HelloApplication.class, args);
+    }
+}
