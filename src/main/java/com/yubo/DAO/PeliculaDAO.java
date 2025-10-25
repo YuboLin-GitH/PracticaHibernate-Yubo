2bf73df3be4e508402075bc7f12973c2ae80e755
yubo <yubo.lin@educa.jcyl.es>
2025-10-25 22:05:47 +0200

    Echo todos

--- src/main/java/com/yubo/DAO/PeliculaDAO.java
+++ src/main/java/com/yubo/DAO/PeliculaDAO.java
@@ -5,8 +5,12 @@ import org.hibernate.Session;
 import java.util.List;
 
 public interface PeliculaDAO {
+
     void insertarPelicula(Session session, Pelicula p);
+
     void modificarPelicula(Session session, Pelicula p);
+
     void borrarPelicula(Session session, Pelicula p);
+
     List<Pelicula> listarPelicula(Session session);
 }
